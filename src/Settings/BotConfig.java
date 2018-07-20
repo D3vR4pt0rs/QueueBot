bc63eacf4dd440abca00d137c7c1bbb58757dea5
Artem Zaglubotskii <mr_a953@mail.ru>
2018-07-20 22:25:24 +0300

    Update BotConfig.java

--- src/Settings/BotConfig.java
+++ src/Settings/BotConfig.java
@@ -4,5 +4,5 @@ public class BotConfig {
 
     public static final String BOT_USERNAME = "Queue Bot";
 
-    public static final String BOT_TOKEN = "566753071:AAGoLI82C13UGZmfUuygFbQLNq9ec4wpyBE";
+    public static final String BOT_TOKEN = "Your own Token";
 }
